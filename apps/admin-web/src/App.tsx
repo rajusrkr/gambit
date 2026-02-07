d427df458a2579447ff27eeee76fa49ce449a2cc
rajusrkr <rajusarkaroo83@gmail.com>
2026-02-07 19:15:40 +0530

    feat: add approval page

--- apps/admin-web/src/App.tsx
+++ apps/admin-web/src/App.tsx
@@ -3,6 +3,7 @@ import AppLayout from "./pages/app-layout";
 import AppLayout2 from "./pages/app-layout2";
 import Dashboard from "./pages/dashboard";
 import CreateMarket from "./pages/create-market";
+import AdminApproval from "./pages/approval";
 export function App() {
   return (
     <BrowserRouter>
@@ -12,6 +13,7 @@ export function App() {
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/dashboard/create-market" element={<CreateMarket />} />
         </Route>
+        <Route path="/approval" element={<AdminApproval />} />
       </Routes>
     </BrowserRouter>
   );
