3ac7132ea3f271c2666ac133d4060a843b283988
rajusrkr <rajusarkaroo83@gmail.com>
2026-02-07 19:11:26 +0530

    feat: add better auth auth-client

---
+++ apps/admin-web/src/lib/better-auth/auth-client.ts
@@ -0,0 +1,18 @@
+import { createAuthClient } from "better-auth/react";
+import { inferAdditionalFields } from "better-auth/client/plugins";
+
+export const adminAuthClient = createAuthClient({
+  baseURL: "http://localhost:3333/api/admin/auth",
+  plugins: [
+    inferAdditionalFields({
+      user: {
+        approval: {
+          type: ["pending", "approved", "rejected"],
+        },
+        role: {
+          type: ["admin", "superAdmin", "user"],
+        },
+      },
+    }),
+  ],
+});
