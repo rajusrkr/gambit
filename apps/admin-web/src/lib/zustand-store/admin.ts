245055783882bdc37b37f26d8d17588aa243864a
rajusrkr <rajusarkaroo83@gmail.com>
2026-02-07 19:10:13 +0530

    feat: add zustand store for admin

---
+++ apps/admin-web/src/lib/zustand-store/admin.ts
@@ -0,0 +1,41 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+type formTabs = "CT" | "DR" | "O" | "SE" | "P";
+
+interface States {
+  name: string;
+  email: string;
+  role: string;
+  setAdminStates: ({
+    name,
+    email,
+    role,
+  }: {
+    name: string;
+    email: string;
+    role: string;
+  }) => void;
+
+  currentCreateMarketFormTab: formTabs;
+  setCurrentCreateMarketFormTab: (tab: string) => void;
+}
+
+export const useAdminStore = create(
+  persist<States>(
+    (set) => ({
+      email: "",
+      name: "",
+      role: "",
+      currentCreateMarketFormTab: "CT",
+
+      setAdminStates: ({ email, name, role }) => {
+        set({ email, name, role });
+      },
+      setCurrentCreateMarketFormTab: (tab) => {
+        set({ currentCreateMarketFormTab: tab as formTabs });
+      },
+    }),
+    { name: "gambit-admin-store" },
+  ),
+);
